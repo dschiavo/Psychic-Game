c51f86187abbdc9433d31cc17bed1cea2bdffc94
RBG <dschiavo.262@gmail.com>
2018-10-09 16:52:12 -0400

    initial files

---
+++ javascript/game.js
@@ -0,0 +1,43 @@
+//creating variables for wins and losses, they start at 0
+var win = 0;
+var loss = 0;
+
+//creating variable for guesses left, this starts at 9
+var guessesLeft = 9;
+
+//variable for guesses so far
+var guessesSoFar = "";
+
+//variable for choices
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+//computer generating random letter
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+console.log(computerChoice);
+
+//user guessing letter and comparing to computer selected letter
+
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        win++;
+    }else{
+        guessesLeft--;
+    }
+
+    if(guesses === 0){
+        loss++
+    }
+
+    //creating variables to hold references
+    var winText = document.getElementById('wins');
+    var lossText = document.getElementById('losses');
+    var guessesLeftText = document.getElementById('guesses');
+    var guessesSoFar = document.getElementById("guessesSoFar");
+
+    //display results
+    <p id = wintext>winText.innerHTML = "Wins: " + wins</p> ;
+    lossText.innerHTML = "Losses: " +loss;
+    guessesLeftText.innerHTML = "Guesses Left: " + guessesLeft;
